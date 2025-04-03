0d4e4c51b327da375cd65ea22befff132061e272
fushaolei <1563250958@qq.com>
2025-04-03 15:12:43 +0800

    update blog

---
+++ js/common.js
@@ -0,0 +1,9 @@
+const themeObjectArray = [
+    { theme: 'light', name: '白天' },
+    { theme: 'dark', name: '黑夜' },
+    { theme: 'cyberpunk', name: '赛博朋克' },
+    { theme: 'autumn', name: '秋天' },
+    { theme: 'daylight', name: '日光' },
+    { theme: 'mint', name: '薄荷' },
+    { theme: 'souffle', name: '舒芙蕾' }
+];
