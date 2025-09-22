21996b21ff283d254f965d345211997475b52e07
fushaolei <1563250958@qq.com>
2025-09-22 16:28:24 +0800

    update blog

---
+++ js/common.js
@@ -0,0 +1,6 @@
+const themeObjectArray = [
+    { theme: 'light', name: '白天' },
+    { theme: 'dark', name: '黑夜' },
+    { theme: 'cyberpunk', name: '赛博朋克' },
+    { theme: 'pxxnhub', name: '学习网站' }
+];
